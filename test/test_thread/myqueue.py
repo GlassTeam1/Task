1a446a3f206ad2d18d7abc729490e22eb8eefe12
Mr.Zhang <zhangmiao.gm.cn@gmail.com>
2018-11-13 14:59:13 +0800

    测试一些东西（不重要的提交）

--- test/test_thread/myqueue.py
+++ test/test_thread/myqueue.py
@@ -10,12 +10,15 @@ class Producer(threading.Thread):
         self.data = queue
 
     def run(self):
-        for i in range(5):
-            print("%s:%s is producing %d to the queue!\n" %(time.ctime(),self.getName(),i))
-
+        filename = "H:\\task\\Task\\bytes_data_2018年11月5日.txt"
+        my_open = open(filename,'rb')
+        i =0
+        while True:
+            #bytes = my_open.read(39)
+            #str = len(bytes)
+            i= i+1
             self.data.put(i)
-            time.sleep(random.randrange(10)/5)
-        print("%s:%s finished!"%(time.ctime(),self.getName()))
+            time.sleep(1)
 
 class Consumer(threading.Thread):
     def __init__(self,t_name,queue):
@@ -23,26 +26,18 @@ class Consumer(threading.Thread):
         self.data = queue
 
     def run(self):
-        for i in range(5):
+        while True:
             val = self.data.get()
-
-            print("%s:%s is consuming, %d in the queue is condumed!\n"%(time.ctime(),
-                                                                        self.getName(),
-                                                                        val))
-            time.sleep(random.randrange(10))
-        print("%s:%s finished"%(time.ctime(),self.getName()))
+            #print("当前队列长度：%d"%(self.data.qsize))
+            print(val)
+            time.sleep(0.01)
 
 def main():
-    queue = Queue()
+    queue = Queue(maxsize=10)
     producer = Producer('Pro.',queue)
     consumer = Consumer('Con.',queue)
     producer.start()
     consumer.start()
-
-    producer.join()
-    consumer.join()
-
-    print('all thread terminate!')
 
 if __name__ == '__main__':
     main()
