51be9ea26210d7f1530b513e5e457fc4d45df84f
FlyChenKai <1628343148@qq.com>
2018-11-21 23:24:03 +0800

    改下主页面

--- v1/connectMain.py
+++ v1/connectMain.py
@@ -1,28 +1,19 @@
 # -*- coding:utf-8 -*-
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import windowTest
+import mainwindow2
 import print
 import sys
 
-
-class Main(QMainWindow, windowTest.Ui_MainWindow2):
+class Main(QMainWindow, mainwindow2.Ui_MainWindow2):
     def __init__(self):
         super(Main, self).__init__()
         self.setupUi(self)
 
 
-
-class Child(QMainWindow, print.Ui_MainWindow):
+class Child(QMainWindow, print.Ui_MainWindow10):
     def __init__(self):
-        # super(Child, self).__init__()
-        # self.setupUi(self)
-        # self.producer.start()
-        # self.consumer.start()
-        # self.timer.start()
-        # sys.exit(app.exec_())
         super(Child, self).__init__()
         self.setupUi(self)
-
         self.producer.start()
         self.consumer.start()
         self.timer.start()
@@ -33,8 +24,8 @@ class Child(QMainWindow, print.Ui_MainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main = Main()
-    ch = Child()
+    main=Main()
     main.show()
+    ch=Child()
     main.pushButton1.clicked.connect(ch.OPEN)
     sys.exit(app.exec_())
